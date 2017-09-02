bea41db14ee470352a5ce05225626116a3ffdfb0
Daniel Hill <danielhill10@bellsouth.net>
2017-09-02 13:01:41 -0400

    Changed Camera Position and Viewport properties to be consistent with C# 3.0 sytnax

--- NECromanceR/Camera.cs
+++ NECromanceR/Camera.cs
@@ -9,22 +9,26 @@ using Microsoft.Xna.Framework;
 namespace NECromanceR {
     class Camera {
 
-        private Vector2 Position { get { return Position; } set { Position = value; } }
-        private Vector2 Viewport { get { return Viewport; } set { Viewport = value; } }
+        /* These are C# properties - 
+           behind the scenes, the compiler is generating
+           two fields called 'position' and 'viewport' and 
+           getters and setters for both. */
+        private Vector2 Position { get; set; }
+        private Vector2 Viewport { get; set; }
 
         public Camera ( Vector2 position, Vector2 viewport ) {
-            this.Position = position;
-            this.Viewport = viewport;
+            Position = position;
+            Viewport = viewport;
         }
 
         public Camera ( int x, int y, Vector2 viewport ) {
-            this.Position = new Vector2( x, y );
+            Position = new Vector2( x, y );
             this.Viewport = viewport;
         }
 
         public Camera ( int x, int y, int width, int height ) {
-            this.Position = new Vector2( x, y );
-            this.Viewport = new Vector2( width, height );
+            Position = new Vector2( x, y );
+            Viewport = new Vector2( width, height );
         }
 
         public void LookAt ( Vector2 worldCoordinates ) {

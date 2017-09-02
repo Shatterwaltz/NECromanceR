300119d9ff7aa2ac88a9cbfde6a92be9075eddb7
Ben <shatterwaltz@gmail.com>
2017-09-02 01:10:18 -0400

    changed position and viewport properties to public

--- NECromanceR/Camera.cs
+++ NECromanceR/Camera.cs
@@ -1,34 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace NECromanceR {
     class Camera {
 
-        private Vector2 Position { get { return Position; } set { Position = value; } }
-        private Vector2 Viewport { get { return Viewport; } set { Viewport = value; } }
+        public Vector2 Position;
+        public Vector2 Viewport;
 
-        public Camera ( Vector2 position, Vector2 viewport ) {
+        public Camera(Vector2 position, Vector2 viewport) {
             this.Position = position;
             this.Viewport = viewport;
         }
 
-        public Camera ( int x, int y, Vector2 viewport ) {
-            this.Position = new Vector2( x, y );
+        public Camera(int x, int y, Vector2 viewport) {
+            this.Position = new Vector2(x, y);
             this.Viewport = viewport;
         }
 
-        public Camera ( int x, int y, int width, int height ) {
-            this.Position = new Vector2( x, y );
-            this.Viewport = new Vector2( width, height );
+        public Camera(int x, int y, int width, int height) {
+            this.Position = new Vector2(x, y);
+            this.Viewport = new Vector2(width, height);
         }
 
-        public void LookAt ( Vector2 worldCoordinates ) {
-            Position = new Vector2( worldCoordinates.X - Viewport.X/2, worldCoordinates.Y - Viewport.Y/2 );
+        //After LookAt is called on something's position, camera's position can be used
+        //to calculate screen coordinates by subtracting it from an entity's position.
+        public Vector2 LookAt(Vector2 worldCoordinates) {
+            Position = new Vector2(worldCoordinates.X - Viewport.X / 2, worldCoordinates.Y - Viewport.Y / 2);
+            return Position;
         }
 
     }

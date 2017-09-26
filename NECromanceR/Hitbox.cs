a4aeb9d66e75ffde43f16ca9617b9cd7d3483bfe
Ben <shatterwaltz@gmail.com>
2017-09-26 13:41:04 -0400

    Hitbox class, basic as it gets

---
+++ NECromanceR/Hitbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+namespace NECromanceR {
+    class Hitbox {
+        public Rectangle HitboxRect { get; set; }
+
+        public Hitbox(Rectangle hitboxRect) {
+            HitboxRect = hitboxRect;
+        }
+    }
+}
